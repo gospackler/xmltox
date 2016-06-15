866f8fd9f1e5a6341ba3d846b51be60d2b5053a3
george <george.thomas@hart.com>
2016-06-15 14:29:53 -0700

    Saving

--- test/test.go
+++ test/test.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
 	"fmt"
-	"image/png"
 	"io/ioutil"
+
+	"github.com/jung-kurt/gofpdf"
 )
 
-// Use a reader for this to negate huge slice created.
+// Use a reader for this to negage slice created.
+// Using the make is suicide with a number passed.
 func decodeBase64(base64Data []byte) (dst []byte) {
 
 	dst = make([]byte, 1000000)
@@ -20,8 +23,26 @@ func decodeBase64(base64Data []byte) (dst []byte) {
 	return
 }
 
+// Return the pdf data in bytes.
+// Ideally put the png into the pdf and generate it.
+func getPdf(pngData []byte) ([]byte, error) {
+	pngReader := bytes.NewReader(pngData)
+	pdf := gofpdf.New("P", "mm", "A4", "")
+	options := gofpdf.ImageOptions{
+		ReadDpi:   false,
+		ImageType: "PNG",
+	}
+	imageName := "info"
+	pdf.RegisterImageOptionsReader(imageName, options, pngReader)
+	pdf.ImageOptions(imageName, 0, 0, 10, 10, true, options, 1, "")
+
+	pdf.OutputFileAndClose("goabc.pdf")
+	return nil, nil
+}
+
 func main() {
 	base64Data, _ := ioutil.ReadFile("base64encode")
 	pngData := decodeBase64(base64Data)
 	ioutil.WriteFile("goabc.png", pngData, 0644)
+	getPdf(pngData)
 }
